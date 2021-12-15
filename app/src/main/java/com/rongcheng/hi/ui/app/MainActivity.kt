8e49b520229d4bbbe5861a89125629679457cd5b
fengrongcheng <fengrongcheng@360.cn>
2021-12-15 16:52:10 +0800

    修改demo

--- app/src/main/java/com/rongcheng/hi/ui/app/MainActivity.kt
+++ app/src/main/java/com/rongcheng/hi/ui/app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.rongcheng.hi.ui.app
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,13 +10,15 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.ImageView
 import com.rongcheng.hi.ui.app.databinding.ActivityMainBinding
+import com.rongcheng.hi.ui.app.tab.HiTabBottomDemoActivity
+import com.rongcheng.hi.ui.app.tab.HiTabTopDemoActivity
 import com.rongcheng.hi.ui.tab.bottom.HiTabBottom
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,38 +26,20 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.refresh.setOnClickListener(this)
+        binding.tabBottomLayout.setOnClickListener(this)
+        binding.tabTopLayout.setOnClickListener(this)
+    }
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+    override fun onClick(v: View?) {
+        when (v) {
+            binding.tabBottomLayout -> startActivity(
+                Intent(
+                    this,
+                    HiTabBottomDemoActivity::class.java
+                )
+            )
+            binding.tabTopLayout -> startActivity(Intent(this, HiTabTopDemoActivity::class.java))
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
